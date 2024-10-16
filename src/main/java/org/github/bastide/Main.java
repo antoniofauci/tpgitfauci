2dacb04cddc62b3921808897bcd9293c7a28a23f
Lina Nafaa <lina.nafaa@etud.univ-jfc.fr>
2024-10-16 10:22:54 +0200

    commit lina

--- src/main/java/org/github/bastide/Main.java
+++ src/main/java/org/github/bastide/Main.java
@@ -1,4 +1,5 @@
 package org.github.bastide;
+//modification lina
 //TEST
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
